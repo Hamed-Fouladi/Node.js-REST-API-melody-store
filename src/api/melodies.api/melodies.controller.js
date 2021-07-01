2d0e91c850e6ef8b7d78116f6b036b5df2b2b86c
Fouladi, Hamed <hfouladi@hq.telesens.lan>
2021-07-01 10:32:50 +0300

    maked get melodies task

--- src/api/melodies.api/melodies.controller.js
+++ src/api/melodies.api/melodies.controller.js
@@ -1,4 +1,4 @@
-const { categories, genres } = require('../../../database/models');
+const { categories, genres, melodies, categories_melodies } = require('../../../database/models');
 
 module.exports = {
   getGenresWithCategories: (req, res) => {
@@ -9,5 +9,19 @@ module.exports = {
       .catch((error) => {
         res.status(500).send({ message: error.message });
       });
+  },
+  getMelodies: async (req, res) => {
+    try {
+      const { id } = req.params;
+      if (isNaN(id)) return res.status(400).send('"category id" should be a number');
+      const categoryMelodies = await categories_melodies.findAll({ where: { fk_categories_id: id } });
+      const melodiesId = categoryMelodies.map((melody) => melody.fk_melodies_id);
+
+      const result = await melodies.findAll({ where: { id: melodiesId } });
+      return res.status(200).json({ Melodies: result });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: error.message });
+    }
   },
 };
--- src/api/melodies.api/melodies.router.js
+++ src/api/melodies.api/melodies.router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const {
   getGenresWithCategories,
+  getMelodies,
 } = require('./melodies.controller');
 const {
   jwtVerify,
@@ -9,5 +10,6 @@ const {
 
 const baseRouterPath = '/melodies';
 router.get(`${baseRouterPath}/genres-and-categories`, jwtVerify, getGenresWithCategories);
+router.get(`${baseRouterPath}/category-id/:id`, jwtVerify, getMelodies);
 
 module.exports = router;
