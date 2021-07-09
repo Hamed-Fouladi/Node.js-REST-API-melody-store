db8bfd81349789f5190574cfbb58dd7f056267ef
Fouladi, Hamed <hfouladi@hq.telesens.lan>
2021-07-09 11:54:43 +0300

    maked changePassword method

--- src/api/user.api/user.controller.js
+++ src/api/user.api/user.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const { users } = require('../../../database/models');
 
 module.exports = {
@@ -13,5 +14,21 @@ module.exports = {
     }).catch((error) => {
       res.status(500).send({ message: error.message });
     });
+  },
+  changePassword: async (req, res) => {
+    try {
+      const user = await users.findOne({
+        where: {
+          id: req.user.userId,
+        },
+      });
+      await user.update({
+        password: bcrypt.hashSync(req.body.password, 8),
+      });
+      res.status(200).send({ message: 'password was changed successfully' });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: error.message });
+    }
   },
 };
--- src/api/user.api/user.router.js
+++ src/api/user.api/user.router.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
 
 const {
-  getProfileData
+  getProfileData,
+  changePassword,
 } = require('./user.controller');
 const {
   jwtVerify,
+  passwordValidation,
+  validate,
 } = require('../../middlewares');
 
 const baseRouterPath = '/user';
 router.get(`${baseRouterPath}/profile`, jwtVerify, getProfileData);
+router.post(`${baseRouterPath}/change-password`, jwtVerify, passwordValidation(), validate, changePassword);
 
 module.exports = router;
